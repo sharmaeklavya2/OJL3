45320042ccfd48f9ca451b0d9f467a7f0873e2b0
Eklavya Sharma <eklavya@example.com>
2015-11-03 00:09:30 +0530

    OJL3 now runs executables and parses report
    Formerly, OJL3 could only run scripts. The interpreter name had to be provided as well. Now OJL3 can run any executable. To do that, set the interpreter name to None.
    
    OJL3 now reads report.txt generated by safeexec and uses it to determine verdict, execution time and memory usage

--- OJL3.py
+++ OJL3.py
@@ -6,12 +6,16 @@ SANDBOX_PATH = os.path.join(OJL3_DIR, "OJ_sandbox")
 SAFEEXEC_PATH = os.path.join(OJL3_DIR, "safeexec")
 INPR_PATH = os.path.join(OJL3_DIR, "inprs")
 
-SAFEEXEC_ARGS = ["--gid", "10000", "--nproc", "30"]
+SAFEEXEC_ARGS = ["--gid", "10000", "--nproc", "5"]
 
-def run_script(inpr_path, source_path, in_path=None, inpr_options=None, exec_path=None, report_path=None, time_lim_s=10, mem_lim_k=50000):
-	if not inpr_options:
-		inpr_options = []
-	cmdline = [inpr_path] + inpr_options + [source_path]
+def run_prog(inpr_path, source_path, in_path=None, cmd_options=None, exec_path=None, report_path=None, time_lim_s=10, mem_lim_k=80000):
+	# if inpr_path is None, source_path will be treated as an executable
+	if not cmd_options:
+		cmd_options = []
+	if inpr_path:
+		cmdline = [inpr_path] + cmd_options + [source_path]
+	else:
+		cmdline = [source_path] + cmd_options
 
 	if report_path:
 		report_args = ["--report_file", os.path.abspath(report_path)]
@@ -25,4 +29,21 @@ def run_script(inpr_path, source_path, in_path=None, inpr_options=None, exec_pat
 
 	sp = subprocess.Popen(run_path, stdin=file_obj, cwd=exec_path, stdout=subprocess.PIPE, stderr=subprocess.PIPE, universal_newlines=True)
 	(out,err) = sp.communicate()
-	return (sp.returncode, out, err)
+	verdict, time_s, mem_k = parse_report(open(report_path).read())
+	return (verdict, out, err, time_s, mem_k)
+
+def parse_report(report_text):
+	report_lines = report_text.strip().split('\n')
+	if report_lines[0] == "OK":
+		verdict = "OK"
+	elif report_lines[0] == "Time Limit Exceeded":
+		verdict = "TLE"
+	elif report_lines[0].startswith("Command exited with non-zero status"):
+		verdict = "NZEC"
+	elif report_lines[0].startswith("Command terminated by signal"):
+		verdict = report_lines[0].rsplit(maxsplit=1)[1][:-1]
+	else:
+		raise Exception("Unknown verdict "+report_lines[0])
+	mem_k = int(report_lines[2].split()[2])
+	time_s = float(report_lines[3].split()[2])
+	return (verdict, time_s, mem_k)
